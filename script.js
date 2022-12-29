b123994539a4094e3da7bb241b2fc9d2dfb55bda
Kento225 <augustaskiela899@gmail.com>
2022-12-29 18:28:01 +0200

    Add functionality to calculate entered numbers, add the ability to delete elements from display

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-displayText = document.createElement("div")
 display = document.querySelector(".display")
-display.appendChild(displayText)
 numberButtons = document.querySelector(".numbers")
 operators = document.querySelector(".operators")
+equal = document.querySelector(".equal")
+ac = document.querySelector(".ac")
+c = document.querySelector(".c")
 let textInDisplay = {
     firstNumber : "",
     operator : "",
@@ -12,7 +13,7 @@ let textInDisplay = {
 numberButtons.addEventListener("click",(e) => {
     console.log(textInDisplay)
 
-    if(e.target.value === undefined || e.target.value === "equal"){
+    if(e.target.value === undefined || e.target.classList === "equal"){
         return
     }
     if(textInDisplay.operator.length > 0){
@@ -23,22 +24,68 @@ numberButtons.addEventListener("click",(e) => {
         display.textContent += e.target.value
         return textInDisplay
     }
-
-    if(displayText.textContent > 0){
+    if(display.textContent > 0){
         if(textInDisplay.firstNumber.length > 10){
             return
         }
-        displayText.textContent = textInDisplay.firstNumber + e.target.classList
-        textInDisplay.firstNumber += e.target.classList
+        display.textContent = textInDisplay.firstNumber + e.target.value
+        textInDisplay.firstNumber += e.target.value
     }else{
-        displayText.textContent = e.target.classList
-        textInDisplay.firstNumber += e.target.classList
+        display.textContent = e.target.value
+        textInDisplay.firstNumber += e.target.value
         return textInDisplay
     }
 })
 operators.addEventListener("click",(e) => {
-    displayText.textContent +=` ${e.target.value} `
-    textInDisplay.operator = e.target.value
+    if(textInDisplay.operator.length > 0){
+        result = calculations(textInDisplay.operator)
+        textInDisplay.firstNumber = result
+        display.textContent = result
+        textInDisplay.operator = e.target.value
+        display.textContent +=` ${e.target.value} `
+        textInDisplay.lastNumber = ""
+    }else{
+    textInDisplay.operator += e.target.value
+    display.textContent +=` ${e.target.value} `
     return textInDisplay
+}})
+equal.addEventListener("click", (e) =>{
+    result = calculations(textInDisplay.operator)
+    if(result === undefined){
+        textInDisplay.firstNumber = ""
+    }
+    console.log(result)
+    display.textContent = result
+    textInDisplay.firstNumber = result
+    textInDisplay.lastNumber = ""
+    textInDisplay.operator = ""
+
 })
 
+
+function calculations(operator) {
+ switch (operator){
+    case "/" : return textInDisplay.firstNumber/textInDisplay.lastNumber
+    case "*" : return textInDisplay.firstNumber*textInDisplay.lastNumber
+    case "+" : return (textInDisplay.firstNumber-0)+(textInDisplay.lastNumber-0)
+    case "-" : return textInDisplay.firstNumber-textInDisplay.lastNumber
+}}
+ac.addEventListener("click", (e) => {
+    display.textContent = ""
+    textInDisplay.firstNumber = ""
+    textInDisplay.operator = ""
+    textInDisplay.lastNumber = ""
+
+})
+c.addEventListener("click", (e)=>{
+    currentDisplay = display.textContent.slice(0, -1)
+    display.textContent = currentDisplay
+    if(textInDisplay.lastNumber.length > 0){
+        textInDisplay.lastNumber = textInDisplay.lastNumber.slice(0, -1)
+    }else if(textInDisplay.operator.length > 0){
+        textInDisplay.operator = textInDisplay.operator.slice(0, -1)
+    }else{
+        textInDisplay.firstNumber = textInDisplay.firstNumber.slice(0, -1)
+    }
+
+})
