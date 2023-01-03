a0f73c3e1a02e15b9f186b9c9ce4f054be671c1e
Kento225 <augustaskiela899@gmail.com>
2023-01-03 18:01:20 +0200

    Change style, fix bugs, add keyboard support

--- script.js
+++ script.js
@@ -4,62 +4,105 @@ operators = document.querySelector(".operators")
 equal = document.querySelector(".equal")
 ac = document.querySelector(".ac")
 c = document.querySelector(".c")
+dot = document.querySelector(".dot")
+const numBtnArr = ["1","2","3","4","5","6","7","8","9","0"]
+const opBtnArr = ["/","*","-","+"]
 let textInDisplay = {
     firstNumber : "",
     operator : "",
-    lastNumber : "",
+    lastNumber : ""
+}
+
+function deletion(){
+    if(textInDisplay.lastNumber.length > 0){
+        display.textContent = display.textContent.slice(0,-1)
+        textInDisplay.lastNumber = textInDisplay.lastNumber.slice(0, -1)
+    }else if(textInDisplay.operator.length > 0){
+        display.textContent = display.textContent.slice(0,-1)
+        textInDisplay.operator = textInDisplay.operator.slice(0, -1)
+    }else{
+        display.textContent = display.textContent.slice(0,-1)
+        textInDisplay.firstNumber = textInDisplay.firstNumber.slice(0, -1)
+    }
+    }
+function dotLogic(){
+    if(textInDisplay.operator.length <= 0){
+        if(textInDisplay.firstNumber.includes(".")){
+            return
+    }
+    textInDisplay.firstNumber += "."
+    display.textContent += "."
+    }
+    if(textInDisplay.operator.length > 0){
+        if(textInDisplay.lastNumber.includes(".")){
+            return
+    }
+    textInDisplay.lastNumber += "."
+    display.textContent += "."
+    }
+}
+
+function displayText(text){
+    if(textInDisplay.firstNumber.length > 9){
+        return
+    }
+        if(textInDisplay.operator.length <= 0 && textInDisplay.lastNumber.length <= 0){
+            display.textContent += text.toString()
+            textInDisplay.firstNumber += text.toString()
+    }
+    if(textInDisplay.operator.length > 0){
+        display.textContent += text.toString()
+        textInDisplay.lastNumber += text.toString()
+    }
+}
+
+function operatorLogic(callback){
+    if(textInDisplay.operator.length > 0){
+        result = calculations(textInDisplay.operator)
+        textInDisplay.firstNumber = result.toString()
+        display.textContent = result.toString()
+        textInDisplay.operator = callback.toString()
+        display.textContent +=`${callback}`
+        textInDisplay.lastNumber = ""
+    }else{
+    textInDisplay.operator += callback
+    display.textContent +=`${callback}`
+    return textInDisplay
+}}
+
+function pressEqual(){
+    if(textInDisplay.operator === "/" && (textInDisplay.firstNumber === "0" || textInDisplay.lastNumber === "0")){
+        alert("What do you think you're doing?!")
+        return
+    }
+    if(textInDisplay.operator.length <= 0 || textInDisplay.firstNumber.length <= 0 || textInDisplay.lastNumber.length <= 0){
+        return
+    }
+    result = calculations(textInDisplay.operator)
+    result = Math.floor(result * 1000) / 1000;
+    if(result === undefined ){
+        return
+    }
+    console.log(result)
+    textInDisplay.firstNumber = result.toString()
+    display.textContent = result.toString()
+    textInDisplay.lastNumber = ""
+    textInDisplay.operator = ""
 }
 
 numberButtons.addEventListener("click",(e) => {
     console.log(textInDisplay)
-
     if(e.target.value === undefined || e.target.classList === "equal"){
         return
     }
-    if(textInDisplay.operator.length > 0){
-        if(textInDisplay.lastNumber.length > 10){
-            return
-        }
-        textInDisplay.lastNumber += e.target.value
-        display.textContent += e.target.value
-        return textInDisplay
-    }
-    if(display.textContent > 0){
-        if(textInDisplay.firstNumber.length > 10){
-            return
-        }
-        display.textContent = textInDisplay.firstNumber + e.target.value
-        textInDisplay.firstNumber += e.target.value
-    }else{
-        display.textContent = e.target.value
-        textInDisplay.firstNumber += e.target.value
-        return textInDisplay
-    }
+    displayText(e.target.value)
+
 })
 operators.addEventListener("click",(e) => {
-    if(textInDisplay.operator.length > 0){
-        result = calculations(textInDisplay.operator)
-        textInDisplay.firstNumber = result
-        display.textContent = result
-        textInDisplay.operator = e.target.value
-        display.textContent +=` ${e.target.value} `
-        textInDisplay.lastNumber = ""
-    }else{
-    textInDisplay.operator += e.target.value
-    display.textContent +=` ${e.target.value} `
-    return textInDisplay
-}})
-equal.addEventListener("click", (e) =>{
-    result = calculations(textInDisplay.operator)
-    if(result === undefined){
-        textInDisplay.firstNumber = ""
-    }
-    console.log(result)
-    display.textContent = result
-    textInDisplay.firstNumber = result
-    textInDisplay.lastNumber = ""
-    textInDisplay.operator = ""
-
+    operatorLogic(e.target.value)
+})
+equal.addEventListener("click", () =>{
+    pressEqual()
 })
 
 
@@ -75,17 +118,35 @@ ac.addEventListener("click", (e) => {
     textInDisplay.firstNumber = ""
     textInDisplay.operator = ""
     textInDisplay.lastNumber = ""
+    result = ""
 
 })
 c.addEventListener("click", (e)=>{
-    currentDisplay = display.textContent.slice(0, -1)
-    display.textContent = currentDisplay
-    if(textInDisplay.lastNumber.length > 0){
-        textInDisplay.lastNumber = textInDisplay.lastNumber.slice(0, -1)
-    }else if(textInDisplay.operator.length > 0){
-        textInDisplay.operator = textInDisplay.operator.slice(0, -1)
-    }else{
-        textInDisplay.firstNumber = textInDisplay.firstNumber.slice(0, -1)
+    deletion()
+})
+dot.addEventListener("click", (e) =>{
+    dotLogic()
+})
+addEventListener("keydown",(e) => {
+    console.log(textInDisplay)
+    if(numBtnArr.includes(e.key)){
+        displayText(e.key)
+    }
+    if(opBtnArr.includes(e.key)){
+        operatorLogic(e.key)
+    }
+    if(e.key === "Enter"){
+        e.preventDefault()
+        pressEqual()
+    }
+    if(e.key === "."){
+        dotLogic()
+    }
+    if(e.key === "Backspace"){
+        deletion()
+    }
+    if(e.key === "/"){
+        e.preventDefault()
     }
 
 })
